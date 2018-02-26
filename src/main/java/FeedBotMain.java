33983a5a8ef052463c0964a6948ebb8bc9d151e1
Oleksandr Konstantinov <iamkonst17@gmail.com>
2018-02-26 13:37:52 +0200

    Initial commit

---
+++ src/main/java/FeedBotMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by OleksandrKonstantinov on 2/26/18.
+ */
+public class FeedBotMain {
+
+    public static void main(String[] args) {
+        System.out.print("Hello Bot");
+    }
+}
