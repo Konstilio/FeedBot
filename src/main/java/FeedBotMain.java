c1aa9264a9480510c812939051af011cdc52dc0d
Oleksandr Konstantinov <iamkonst17@gmail.com>
2018-02-27 10:02:18 +0200

    add echo bot

--- src/main/java/FeedBotMain.java
+++ src/main/java/FeedBotMain.java
@@ -1,9 +1,23 @@
+import TelegramPolling.LongPollingBot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
 /**
  * Created by OleksandrKonstantinov on 2/26/18.
  */
 public class FeedBotMain {
 
     public static void main(String[] args) {
-        System.out.print("Hello Bot");
+
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new LongPollingBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/TelegramPolling/LongPollingBot.java
@@ -0,0 +1,34 @@
+package TelegramPolling;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.List;
+
+public class LongPollingBot extends TelegramLongPollingBot {
+
+    public String getBotToken() {
+        return "566645248:AAHZc8KLccbnhiuS8FZPXtFSCcJ-G78GuBA";
+    }
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                System.out.println(message);
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "HPMTestFeedBot";
+    }
+
+}
